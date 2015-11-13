06439bd4f22c0642719e95a68d27cd6b210f1bbc
Eva J <evka.jelinkova@gmail.com>
2015-11-13 17:12:54 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,4 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post':post})
